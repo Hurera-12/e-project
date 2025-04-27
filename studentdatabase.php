ed2e8aadc795bc7598f1094bb11c1588a0bb7efc
duii78younus <pc3734.duayounus@gmail.com>
2025-04-27 14:42:28 +0500

    studentdatabase

---
+++ studentdatabase.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$db="studentborrow_records";
+
+$conn=new mysqli($servername,$username,$password,$db);
+
+// if($conn){
+//     echo "database connected";
+// } else {
+//     echo "not connected";
+// }
+?>
